cf38b591cddfe831274f9daedebdce935155b08a
federico.cacco <federico.cacco@e6631615-25d8-7dd5-546a-497e588fe4f1>
2010-11-28 20:42:53 +0000

    Prime prove per interfaccia grafica

--- trunk/CAClient/src/Prova.java
+++ trunk/CAClient/src/Prova.java
@@ -1,4 +1,4 @@
-import org.eclipse.swt.SWT;
+//import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
---
+++ trunk/CAClient/src/ProvaSWT.java
@@ -0,0 +1,122 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.DateTime;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.nebula.widgets.datechooser.DateChooser;
+import org.eclipse.nebula.widgets.gallery.Gallery;
+import org.eclipse.nebula.widgets.gallery.DefaultGalleryItemRenderer;
+import org.eclipse.nebula.widgets.gallery.DefaultGalleryGroupRenderer;
+import org.eclipse.swt.browser.Browser;
+
+
+public class ProvaSWT {
+	
+	
+
+	protected Shell shlCaclient;
+	protected Shell shlCaclient2;
+
+	
+
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			ProvaSWT window = new ProvaSWT();
+			window.open();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Open the window.
+	 */
+	public void open() {
+		Display display = Display.getDefault();
+		createContents();
+		shlCaclient.open();
+		shlCaclient.layout();
+		while (!shlCaclient.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	/**
+	 * Create contents of the window.
+	 */
+	protected void createContents() {
+		shlCaclient = new Shell();
+		shlCaclient.setSize(332, 142);
+		shlCaclient.setText("CAClient");
+		shlCaclient.setLayout(new GridLayout(5, false));
+		new Label(shlCaclient, SWT.NONE);
+		
+		Label lblScegliUnaOperazione = new Label(shlCaclient, SWT.NONE);
+		lblScegliUnaOperazione.setText("SCEGLI UNA OPERAZIONE:");
+		new Label(shlCaclient, SWT.NONE);
+		new Label(shlCaclient, SWT.NONE);
+		new Label(shlCaclient, SWT.NONE);
+		new Label(shlCaclient, SWT.NONE);
+		
+		Label lblCreaUnNuovo = new Label(shlCaclient, SWT.NONE);
+		lblCreaUnNuovo.setText("1- Crea un nuovo profilo");
+		new Label(shlCaclient, SWT.NONE);
+		new Label(shlCaclient, SWT.NONE);
+		
+		Button btnCrea = new Button(shlCaclient, SWT.NONE);
+		btnCrea.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				creaFile();
+			}
+		});
+		btnCrea.setText("Crea...");
+		new Label(shlCaclient, SWT.NONE);
+		
+		Label lblApriUnProfilo = new Label(shlCaclient, SWT.NONE);
+		lblApriUnProfilo.setText("2- Apri un profilo già esistente");
+		new Label(shlCaclient, SWT.NONE);
+		new Label(shlCaclient, SWT.NONE);
+		
+		Button btnApri = new Button(shlCaclient, SWT.NONE);
+		btnApri.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				caricaFile();
+			}
+		});
+		btnApri.setText("Apri...");
+	}
+		
+	private void creaFile(){
+		FileDialog dialog = new FileDialog(shlCaclient, SWT.SAVE);
+		String result = dialog.open();
+	}
+	
+	private void caricaFile(){
+		FileDialog dialog = new FileDialog(shlCaclient, SWT.OPEN);
+		String result = dialog.open();
+	}
+		
+		
+
+	
+}
